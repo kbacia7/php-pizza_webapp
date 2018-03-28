7f8e64c90caa4663249cb7688a229eb5ce9dc20d
Kamil <kbacia7@gmail.com>
2018-03-28 10:23:33 +0200

    Szablon zakładki "Konfiguracja"

--- admin.php
+++ admin.php
@@ -51,7 +51,19 @@
                                 <a class="nav-link active" data-redirect="home" href="#">Strona główna</a>
                             </li>
                             <li class="nav-item">
+                                <a class="nav-link" data-redirect="config_manager" href="#">Konfiguracja</a>
+                            </li>
+                            <li class="nav-item">
                                 <a class="nav-link" data-redirect="menu_manager" href="#" id="menu">Menu</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-redirect="home" href="#">Galeria</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-redirect="home" href="#">Kontakt</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-redirect="home" href="#">Powiadomienia <span class="badge badge-pill badge-danger">9</span></a>
                             </li>
                         </ul>
                     </div>
@@ -73,86 +85,8 @@
                                         </div>
                                         <p>Ponadto cała komunikacja odbywa się poprzez AJAX, toteż nie jest wymagane przeładowywanie strony!</p>
                                     </div>
-                                    <div id="menu_manager"> <!-- MENU -->
-                                        <h4 class="text-center">Menu</h4>
-                                        <p class="m-0 text-center">W tym miejscu możesz zarządzać menu, zmieniać elementy menu, aktualizować i usuwać!</p>
-                                        <div class="row">
-                                            <div class="col-4 mt-3 global-menu-root menu-category-hidden" style="display: none">
-                                                <ul class="list-group">
-                                                    <li class="list-group-item list-group-item-primary">
-                                                        <span class="menu-title-position">Nazwa menu</span>
-                                                        <input type="text" class="form-control invisible-input collapse menu-title-position-input"/> 
-                                                        <button type="button" class="trash-icon close remove-menu-category-button right-corner" aria-label="Close">
-                                                            <i style="font-size: 0.6em" class="fas fa-trash-alt"></i>
-                                                        </button>      
-                                                        <div class="edit-mode-menu-buttons collapse">
-                                                            <button type="button" class="close menu-title-position-cancel-edit left-corner" aria-label="Close">
-                                                                <i style="font-size: 0.6em" class="fas fa-ban"></i>
-                                                            </button>
-                                                            <button type="button" class="close menu-title-position-save-edit right-corner" aria-label="Close">
-                                                                <i style="font-size: 0.6em" class="fas fa-save"></i>
-                                                            </button>
-                                                        </div>      
-                                                        <div class="remove-menucategory-content collapse">
-                                                            <div class="d-flex h-100 justify-content-center align-items-center">
-                                                                <button type="button" class="no-remove-menucategory btn btn-success btn-sm mr-2">Zostaw</button>
-                                                                <button type="button" class="remove-menucategory btn btn-danger btn-sm">Usuń</button>
-                                                            </div>
-                                                        </div>                                                
-                                                    </li>
-                                                    <li class="list-group-item unused-never-use-menu" style="display: none !important">
-                                                        <button type="button" class="trash-icon close remove-menu-item-button right-corner" aria-label="Close">
-                                                            <i style="font-size: 0.6em" class="fas fa-trash-alt"></i>
-                                                        </button>
-                                                        <div class="edit-mode-menu-buttons collapse">
-                                                            <button type="button" class="close menu-item-cancel-edit left-corner" aria-label="Close">
-                                                                <i style="font-size: 0.6em" class="fas fa-ban"></i>
-                                                            </button>
-                                                            <button type="button" class="close menu-item-save-edit right-corner" aria-label="Close">
-                                                                <i style="font-size: 0.6em" class="fas fa-save"></i>
-                                                            </button>
-                                                        </div>
-                                                        <div class="form-group mb-0">
-                                                            <div class="row mt-0-5">
-                                                                <div class="col-9">
-                                                                    <span class="float-left">
-                                                                        <input type="text" class="form-control invisible-input menu-name-input" value="Nowy element"/>
-                                                                    </span> 
-                                                                </div>
-                                                                <div class="col-3">
-                                                                    <span class="float-right menu-price"><span class="badge badge-pill badge-primary">4.99$</span></span>
-                                                                    <input type="text" class="form-control invisible-input collapse menu-price-input" value="4.99$"/>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                        <div class="remove-menu-content collapse">
-                                                            <div class="d-flex h-100 justify-content-center align-items-center">
-                                                                <button type="button" class="no-remove-menu btn btn-success btn-sm mr-2">Zostaw</button>
-                                                                <button type="button" class="remove-menu btn btn-danger btn-sm">Usuń</button>
-                                                            </div>
-                                                        </div>
-                                                    </li>
-                                                    <li class="list-group-item">
-                                                        <div class="d-flex h-100 justify-content-center align-items-center">
-                                                            <button type="button" class="add-new-menu btn btn-primary btn-sm mr-2">Dodaj nowy element menu</button>
-                                                        </div>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                            <div class="add-new-category-button w-100" style="padding-top: 1%">
-                                                <div class="d-flex h-100 justify-content-center align-items-center">
-                                                    <button type="button" class="add-menu-category btn btn-primary btn-lg">+</button>
-                                                </div>
-                                            </div>
-                                            <div class="w-100" style="padding-top: 1%">
-                                                <div class="d-flex h-100 justify-content-center align-items-center">
-                                                    <h3 class="position-absolute paginator-prev-page" style="top: 0; left: 0;"><</h3>
-                                                    <h3 class="position-absolute paginator-next-page" style="top: 0; right: 0;">></h3>
-                                                </div>
-                                            </div>
-
-                                        </div>
-                                    </div>
+                                    <div id="menu_manager"> <!-- MENU --> </div>
+                                    <div id="config_manager"></div>
                                 </div>
                             </div>
                         </div>
@@ -165,6 +99,8 @@
     </div>
     <script type="text/javascript">
     $(document).ready(function() {
+        $("#config_manager").load("admin_config.php");
+        $("#menu_manager").load("admin_menu.php");
         $("#waitDialog").modal('show');
         errorsTemplatesAjaxLoad().then(function (templates) {
             errorsTemplates = templates;
---
+++ admin_config.php
@@ -0,0 +1,43 @@
+<h4 class="text-center">Konfiguracja</h4>
+<p class="m-0 text-center">W tym miejscu możesz skonfigurować swoją aplikację internetową! Do dzieła!</p>
+<form>
+  <div class="form-group row">
+    <label for="inputName" class="col-sm-2 col-form-label">Nazwa pizzeri</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="inputName" placeholder="Moja Pizzeria">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="inputLocation" class="col-sm-2 col-form-label">Współrzędne pizzerii</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="inputLocation" placeholder="51.801391, 1.118050">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="inputIcon" class="col-sm-2 col-form-label">Ikonka pizzeri</label>
+    <div class="col-sm-10">
+      <input type="file" class="form-control-file" id="inputIcon">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="inputMoney" class="col-sm-2 col-form-label">Waluta</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="inputMoney" placeholder="$">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="inputGallery1">Opis pierwszej galerii</label>
+    <textarea class="form-control" id="inputGallery1" rows="5"></textarea>
+  </div>
+  <div class="form-group row">
+    <label for="inputGallery2">Opis drugiej galerii</label>
+    <textarea class="form-control" id="inputGallery2" rows="5"></textarea>
+  </div>
+  <div class="form-group row">
+    <div class="col-12">
+      <div class="text-center">
+        <button type="submit" class="btn btn-primary">Zapisz</button>
+      </div>
+    </div>
+  </div>
+</form>
