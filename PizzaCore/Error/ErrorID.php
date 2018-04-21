1d84b280412e29d253c26634c62a7783be0e930b
Kamil <kbacia7@gmail.com>
2018-04-21 14:31:10 +0200

    Większe ID dla błędów, rozwiązanie problemu ze wstawianiem nowego błędu

--- PizzaCore/Error/ErrorID.php
+++ PizzaCore/Error/ErrorID.php
@@ -1,7 +1,7 @@
 <?php
 abstract class ErrorID
 {
-	//Menu category
+	//Menu category 
     const MenuCategory_EmptyData = 0;
     const MenuCategory_EmptyTitle = 1;
     const MenuCategory_DoesntExists = 2;
@@ -10,68 +10,68 @@ abstract class ErrorID
     const MenuCategory_CreateComplete = 5;
 	
 	//Menu item
-    const MenuItem_EmptyData = 6;
-    const MenuItem_EmptyTitle = 7;
-    const MenuItem_DoesntExists = 8;
-    const MenuItem_InvalidTitle = 9;
-    const MenuItem_InvalidPrice = 10;
-    const MenuItem_RemoveComplete = 11;
-    const MenuItem_UpdateComplete = 12;
-    const MenuItem_CreateComplete = 13;
+    const MenuItem_EmptyData = 100;
+    const MenuItem_EmptyTitle = 101;
+    const MenuItem_DoesntExists = 102;
+    const MenuItem_InvalidTitle = 103;
+    const MenuItem_InvalidPrice = 104;
+    const MenuItem_RemoveComplete = 105;
+    const MenuItem_UpdateComplete = 106;
+    const MenuItem_CreateComplete = 107;
 	
 	//Config
-    const Config_UpdateComplete = 14;
-    const Config_EmptyPizzeriaName = 15;
-	const Config_InvalidPizzeriaName = 16;
-    const Config_LongPizzeriaName = 17;
-    const Config_EmptyPizzeriaLocation = 18;
-    const Config_InvalidPizzeriaLocation = 19;
-    const Config_EmptyTelephone = 20;
-    const Config_InvalidTelephone = 21;
-    const Config_InvalidIconEx = 22;
-    const Config_InvalidIconSize = 23;
-	const Config_EmptyCurrency = 24;
-    const Config_LongCurrency = 25;
-    const Config_EmptyGalleryOne = 26;
-    const Config_EmptyGalleryTwo = 27;
+    const Config_UpdateComplete = 200;
+    const Config_EmptyPizzeriaName = 201;
+	const Config_InvalidPizzeriaName = 202;
+    const Config_LongPizzeriaName = 203;
+    const Config_EmptyPizzeriaLocation = 204;
+    const Config_InvalidPizzeriaLocation = 205;
+    const Config_EmptyTelephone = 206;
+    const Config_InvalidTelephone = 27;
+    const Config_InvalidIconEx = 208;
+    const Config_InvalidIconSize = 209;
+	const Config_EmptyCurrency = 210;
+    const Config_LongCurrency = 211;
+    const Config_EmptyGalleryOne = 212;
+    const Config_EmptyGalleryTwo = 213;
 	
 	//Gallery
-    const Gallery_EmptyImageDescription = 28;
-    const Gallery_InvalidImageSize = 29;
-    const Gallery_InvalidImageEx = 30;
-    const Gallery_ImageUploadedComplete = 31;
+    const Gallery_EmptyImageDescription = 300;
+    const Gallery_InvalidImageSize = 301;
+    const Gallery_InvalidImageEx = 302;
+    const Gallery_ImageUploadedComplete = 303;
 	
 	//Notification
-	const Notification_SendComplete = 32;
-    const Notification_EmptyTitle = 35;
-    const Notification_EmptyMessage = 37;
-    const Notification_EmptyData = 99;
+	const Notification_SendComplete = 400;
+    const Notification_EmptyTitle = 401;
+    const Notification_EmptyMessage = 402;
+    const Notification_EmptyData = 403;
 
     //User
-    const User_DoesntExists = 38;
-    const User_EmptyData = 39;
-    const User_EmptyAdmin = 40;
-    const User_EmptyFirstName = 41;
-    const User_EmptyLastName = 42;
-    const User_InvalidFirstName = 43;
-    const User_InvalidLastName = 44;
-    const User_EmptyPassword = 45;
-    const User_EmptyLogin = 46;
-    const User_InvalidLogin = 47;
-    const User_CreateComplete = 48;
+    const User_DoesntExists = 500;
+    const User_EmptyData = 501;
+    const User_EmptyAdmin = 502;
+    const User_EmptyFirstName = 503;
+    const User_EmptyLastName = 504;
+    const User_InvalidFirstName = 505;
+    const User_InvalidLastName = 506;
+    const User_EmptyPassword = 507;
+    const User_EmptyLogin = 508;
+    const User_InvalidLogin = 509;
+    const User_CreateComplete = 510;
 
     //Contact Messages
-    const ContactMessage_DoesntExists = 49;
-    const ContactMessage_EmptyMessage = 50;
-    const ContactMessage_EmptyAuthor = 51;
-    const ContactMessage_EmptyRoom = 52;
-    const ContactMessage_CreateComplete = 53;
+    const ContactMessage_DoesntExists = 600;
+    const ContactMessage_EmptyMessage = 601;
+    const ContactMessage_EmptyAuthor = 602;
+    const ContactMessage_EmptyRoom = 603;
+    const ContactMessage_CreateComplete = 604;
 
     //Contact Room
-    const ContactRoom_DoesntExists = 54;
-    const ContactRoom_EmptyTitle = 55;
-    const ContactRoom_EmptyOwner = 56;
-    const ContactRoom_InvalidTitle = 57;
-    const ContactRoom_CreateComplete = 58;
+    const ContactRoom_DoesntExists = 700;
+    const ContactRoom_EmptyTitle = 701;
+    const ContactRoom_EmptyOwner = 702;
+    const ContactRoom_InvalidTitle = 703;
+    const ContactRoom_CreateComplete = 704;
 }
 ?>
--- PizzaJS/Error.js
+++ PizzaJS/Error.js
@@ -1,141 +1,141 @@
 var errorTypes = {
   SUCCESS: 0,
   WARNING: 1,
-  ERROR: 2  
+  ERROR: 2
 };
-var notifyColors = ['success', 'warning', 'danger', 'danger'];
+var notifyColors = ["success", "warning", "danger", "danger"];
 var errorsTemplates = [];
 var errorsId = {
-	//Menu category
-    MenuCategory_EmptyData: 0,
-    MenuCategory_EmptyTitle: 1,
-    MenuCategory_DoesntExists: 2,
-    MenuCategory_RemoveComplete: 3,
-    MenuCategory_UpdateComplete: 4,
-    MenuCategory_CreateComplete: 5,
-	
-	//Menu item
-    MenuItem_EmptyData: 6,
-    MenuItem_EmptyTitle: 7,
-    MenuItem_DoesntExists: 8,
-    MenuItem_InvalidTitle: 9,
-    MenuItem_InvalidPrice: 10,
-    MenuItem_RemoveComplete: 11,
-    MenuItem_UpdateComplete: 12,
-    MenuItem_CreateComplete: 13,
-	
-	//Config
-    Config_UpdateComplete: 14,
-    Config_EmptyPizzeriaName: 15,
-	Config_InvalidPizzeriaName: 16,
-    Config_LongPizzeriaName: 17,
-    Config_EmptyPizzeriaLocation: 18,
-    Config_InvalidPizzeriaLocation: 19,
-    Config_EmptyTelephone: 20,
-    Config_InvalidTelephone: 21,
-    Config_InvalidIconEx: 22,
-    Config_InvalidIconSize: 23,
-	Config_EmptyCurrency: 24,
-    Config_LongCurrency: 25,
-    Config_EmptyGalleryOne: 26,
-    Config_EmptyGalleryTwo: 27,
-	
-	//Gallery
-    Gallery_EmptyImageDescription: 28,
-    Gallery_InvalidImageSize: 29,
-    Gallery_InvalidImageEx: 30,
-    Gallery_ImageUploadedComplete: 31,
-	
-	//Notification
-	Notification_SendComplete: 32,
-    Notification_TitleInvalidChars: 34,
-    Notification_EmptyTitle: 35,
-    Notification_TitleTooLong: 36,
-    Notification_EmptyMessage: 37,
-    Notification_EmptyData: 99,
-    
-    //User
-    User_DoesntExists: 38,
-    User_EmptyData: 39,
-    User_EmptyAdmin: 40,
-    User_EmptyFirstName: 41,
-    User_EmptyLastName: 42,
-    User_InvalidFirstName: 43,
-    User_InvalidLastName: 44,
-    User_EmptyPassword: 45,
-    User_EmptyLogin: 46,
-    User_InvalidLogin: 47,
-    User_CreateComplete: 48,
+  //Menu category
+  MenuCategory_EmptyData: 0,
+  MenuCategory_EmptyTitle: 1,
+  MenuCategory_DoesntExists: 2,
+  MenuCategory_RemoveComplete: 3,
+  MenuCategory_UpdateComplete: 4,
+  MenuCategory_CreateComplete: 5,
 
-    //Contact Messages
-    ContactMessage_DoesntExists: 49,
-    ContactMessage_EmptyMessage: 50,
-    ContactMessage_EmptyAuthor: 51,
-    ContactMessage_EmptyRoom: 52,
-    ContactMessage_CreateComplete: 53,
+  //Menu item
+  MenuItem_EmptyData: 100,
+  MenuItem_EmptyTitle: 101,
+  MenuItem_DoesntExists: 102,
+  MenuItem_InvalidTitle: 103,
+  MenuItem_InvalidPrice: 104,
+  MenuItem_RemoveComplete: 105,
+  MenuItem_UpdateComplete: 106,
+  MenuItem_CreateComplete: 107,
 
-    //Contact Room
-    ContactRoom_DoesntExists: 54,
-    ContactRoom_EmptyTitle: 55,
-    ContactRoom_EmptyOwner: 56,
-    ContactRoom_InvalidTitle: 57,
-    ContactRoom_CreateComplete: 58,
-}
+  //Config
+  Config_UpdateComplete: 200,
+  Config_EmptyPizzeriaName: 201,
+  Config_InvalidPizzeriaName: 202,
+  Config_LongPizzeriaName: 203,
+  Config_EmptyPizzeriaLocation: 204,
+  Config_InvalidPizzeriaLocation: 205,
+  Config_EmptyTelephone: 206,
+  Config_InvalidTelephone: 27,
+  Config_InvalidIconEx: 208,
+  Config_InvalidIconSize: 209,
+  Config_EmptyCurrency: 210,
+  Config_LongCurrency: 211,
+  Config_EmptyGalleryOne: 212,
+  Config_EmptyGalleryTwo: 213,
+
+  //Gallery
+  Gallery_EmptyImageDescription: 300,
+  Gallery_InvalidImageSize: 301,
+  Gallery_InvalidImageEx: 302,
+  Gallery_ImageUploadedComplete: 303,
+
+  //Notification
+  Notification_SendComplete: 400,
+  Notification_EmptyTitle: 401,
+  Notification_EmptyMessage: 402,
+  Notification_EmptyData: 403,
+
+  //User
+  User_DoesntExists: 500,
+  User_EmptyData: 501,
+  User_EmptyAdmin: 502,
+  User_EmptyFirstName: 503,
+  User_EmptyLastName: 504,
+  User_InvalidFirstName: 505,
+  User_InvalidLastName: 506,
+  User_EmptyPassword: 507,
+  User_EmptyLogin: 508,
+  User_InvalidLogin: 509,
+  User_CreateComplete: 510,
+
+  //Contact Messages
+  ContactMessage_DoesntExists: 600,
+  ContactMessage_EmptyMessage: 601,
+  ContactMessage_EmptyAuthor: 602,
+  ContactMessage_EmptyRoom: 603,
+  ContactMessage_CreateComplete: 604,
+
+  //Contact Room
+  ContactRoom_DoesntExists: 700,
+  ContactRoom_EmptyTitle: 701,
+  ContactRoom_EmptyOwner: 702,
+  ContactRoom_InvalidTitle: 703,
+  ContactRoom_CreateComplete: 704
+};
 
 function errorsLoopInit() {
-    setInterval(() => {
-      errorsHandle();        
-    }, 1000);
+  setInterval(() => {
+    errorsHandle();
+  }, 1000);
 }
 
 function errorsHandle() {
-    errorsAjaxLoad().then(function (errors) {
-        $.each(errors, function () {
-            displayError(this);
-        });
+  errorsAjaxLoad().then(function(errors) {
+    $.each(errors, function() {
+      displayError(this);
     });
+  });
 }
 
 function displayError(errorObj) {
-    if(errorObj == undefined) return;
-    $.notify({
-        // options
-        title: "<b>" + errorObj.title + "</b><br/>",
-        message: errorObj.description 
-    },{
-        // settings
-        newest_on_top: true,
-        type: notifyColors[errorObj.code],
-        placement: {
-            from: "top",
-            align: "center"
-        },
-    });
-    
+  if (errorObj == undefined) return;
+  $.notify(
+    {
+      // options
+      title: "<b>" + errorObj.title + "</b><br/>",
+      message: errorObj.description
+    },
+    {
+      // settings
+      newest_on_top: true,
+      type: notifyColors[errorObj.code],
+      placement: {
+        from: "top",
+        align: "center"
+      }
+    }
+  );
 }
 
-function errorsAjaxLoad() { 
-    return new Promise(function(resolve) {
-        $.ajax({
-            url: "PizzaCore/AJAX/Error/errorhandle.php",
-            type: "POST",
-            complete: function (jData) {
-                var jsonRealData = JSON.parse(jData['responseText']);
-                resolve(jsonRealData['errors']);
-            }
-        });
+function errorsAjaxLoad() {
+  return new Promise(function(resolve) {
+    $.ajax({
+      url: "PizzaCore/AJAX/Error/errorhandle.php",
+      type: "POST",
+      complete: function(jData) {
+        var jsonRealData = JSON.parse(jData["responseText"]);
+        resolve(jsonRealData["errors"]);
+      }
     });
+  });
 }
 
-function errorsTemplatesAjaxLoad() { 
-    return new Promise(function(resolve) {
-        $.ajax({
-            url: "PizzaCore/AJAX/Error/errortemplates.php",
-            type: "POST",
-            complete: function (jData) {
-                var jsonRealData = JSON.parse(jData['responseText']);
-                resolve(jsonRealData['templates']);
-            }
-        });
+function errorsTemplatesAjaxLoad() {
+  return new Promise(function(resolve) {
+    $.ajax({
+      url: "PizzaCore/AJAX/Error/errortemplates.php",
+      type: "POST",
+      complete: function(jData) {
+        var jsonRealData = JSON.parse(jData["responseText"]);
+        resolve(jsonRealData["templates"]);
+      }
     });
+  });
 }
